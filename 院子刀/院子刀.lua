a7984b4f69a6b2c6ec8b0dd230a5808cb24abc94
tammmmmm <52206076+tammmmmm@users.noreply.github.com>
2022-05-12 03:57:46 -0400

    Create 院子刀.lua

---
+++ 院子刀/院子刀.lua
@@ -0,0 +1,12 @@
+local plr = game.Players.LocalPlayer
+local chara = plr.Character
+if chara:FindFirstChildOfClass("Humanoid") then chara:FindFirstChildOfClass("Humanoid"):ChangeState(15) end
+chara:ClearAllChildren()
+local newChar = Instance.new("Model")
+newChar.Parent = workspace
+plr.Character = newChar
+wait()
+plr.Character = chara
+newChar:Destroy()
+wait(0.8)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(772, -140, 305)
