5a83d41e4d351819b609ed8be60e3fc532dac4b2
tammmmmm <52206076+tammmmmm@users.noreply.github.com>
2022-05-19 22:35:51 -0400

    Delete wallylib.lua

--- wallylib.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/mAZG02v89Eobn6mUeg9i710QGJz9ANOZ6PgR1aw/i4Wp2q2nDmID6IT1EWNlaGxEVCbotjN0RZiK9O93LcPZ9qoJtJ9RKj3iqxC4noXvQOIVNnnZ7PJXUTMOQMXa8DpnaIB0YUhxnF8P/main/%E4%BB%80%E4%B9%88%E9%AC%BCr2q8IGcBsrThDVyxrkv%E6%AF%8D%E7%8B%97%E7%BB%8A%E5%80%92zdd95Kh7BeUunnxzmTB%E8%82%A5%E7%9A%82%E5%9C%A8%E4%B8%8A%E9%9D%A2.lua"))()
